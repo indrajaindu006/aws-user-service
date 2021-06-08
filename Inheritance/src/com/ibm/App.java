46f47a00e296e79fa946ebb289088992dd1c19e8
001ZQT744 <001ZQT744@LAPTOP-9ODQ3877>
2021-06-08 15:24:05 +0530

    It's my first commit

---
+++ Inheritance/src/com/ibm/App.java
@@ -0,0 +1,39 @@
+package com.ibm;
+
+import com.ibm.model.Employee;
+import com.ibm.model.Engineer;
+import com.ibm.model.Manager;
+
+public class App {
+
+	public static void main(String[] args) {
+		App app=new App();
+		Employee employee=new Manager("Ram", "sri", "ram@gmail.com", 50000, "CSE");
+		System.out.println(employee.getDetails());
+		System.out.println(app.calculateTax(employee));
+		employee=new Engineer("sita", "sreee", "sita@gmail.com", 23000, "ECE");
+		System.out.println(employee.getDetails());
+		System.out.println(app.calculateTax(employee));
+	}
+	
+	
+	public double calculateTax(Employee employee)
+	{
+		double tax=0;
+		if(employee instanceof Manager)
+		{
+			Manager manager=(Manager)employee;
+			tax= (12.0/100)*manager.getSalary();
+		}
+		if(employee instanceof Engineer)
+		{
+			Engineer engineer=(Engineer)employee;
+			
+			tax=(11.9/100)*engineer.getSalary();
+		}
+		return tax;
+	}
+
+
+	}
+
