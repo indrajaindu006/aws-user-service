fbc79e8867db5d902e02bc2ccc331ed7dfb8d689
001ZQT744 <001ZQT744@LAPTOP-9ODQ3877>
2021-06-14 21:19:43 +0530

    Assignment 2(14-6-21)

---
+++ collectionAssignment2/src/com/ibm/Password.java
@@ -0,0 +1,27 @@
+package com.ibm;
+
+import java.util.Scanner;
+
+public class Password {
+	private static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		System.out.println("Enter password");
+		String password = sc.next();
+		boolean check = checkPassword(password);
+		if (check) {
+			System.out.println("Valid");
+		} else
+			System.out.println("Invalid");
+
+	}
+
+	public static boolean checkPassword(String password) {
+		if (password.length() >= 8) {
+			String pattern = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}";
+			return password.matches(pattern);
+		}
+		return false;
+	}
+
+}
